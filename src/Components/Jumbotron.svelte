e11a7859e6e10228d0a017e0bda542d6abb80263
n-marcus <n-marcus@outlook.com>
2023-09-22 11:50:04 +0200

    Working Jumbotron with picture

---
+++ src/Components/Jumbotron.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { currentBeeIndex} from "../stores";
+    import bees from "../bees.json";
+</script>
+
+<div class="jumbotron" style="height:50vh; background-color:black; background-size:contain; background-position: center; background-repeat:no-repeat; background-image:url('./imgs/{bees.bees[$currentBeeIndex].picture_url}')">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+  import { currentBeeIndex } from "../stores.js";
+  import bees from "../bees.json";
+  import Jumbotron from "../Components/Jumbotron.svelte";
+  let beeArray = bees.bees;
+  let currentBeeObject;
+  onMount(() => {
+    console.log(beeArray);
+  });
+
+  function nextBeeButtonClicked() {
+    $currentBeeIndex += 1;
+    $currentBeeIndex %= beeArray.length;
+  }
+</script>
+
+<Jumbotron />
+
+
+
+<div class="debug">
+  {JSON.stringify(beeArray[$currentBeeIndex])}
+
+  Current Bee:
+  <br />
+  {$currentBeeIndex}
+</div>
+
+<button class="btn btn-primary" on:click={nextBeeButtonClicked}> Next Bee</button>
